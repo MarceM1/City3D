b81f041d3f112f76b9f5ea465bb1f85a8f3c8f9d
Marcelo Melogno <marcelomelogno1986@gmail.com>
2023-09-11 20:46:42 -0300

    nueva prueba

--- js/main.js
+++ js/main.js
@@ -136,15 +136,15 @@ function onMouseMove(e) {
 function onDocumentTouchStart(e) {
     if (e.touches.length == 1) {
         e.preventDefault();
-        mouse.x = e.touches[0].pageX - window.innerWidth / 1;
-        mouse.y = e.touches[0].pageY - window.innerHeight / 1;
+        mouse.x = e.touches[0].pageX - window.innerWidth / 0.5;
+        mouse.y = e.touches[0].pageY - window.innerHeight / 0.5;
     };
 };
 function onDocumentTouchMove(e) {
     if (e.touches.length == 1) {
         e.preventDefault();
-        mouse.x = e.touches[0].pageX - window.innerWidth / 1;
-        mouse.y = e.touches[0].pageY - window.innerHeight / 1;
+        mouse.x = e.touches[0].pageX - window.innerWidth / 0.5;
+        mouse.y = e.touches[0].pageY - window.innerHeight / 0.5;
     };
 };
 
